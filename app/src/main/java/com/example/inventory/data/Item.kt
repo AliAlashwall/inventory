ec14f7510768ab7bdeaa3411fa0e317aa53f82fd
Ali <alialashwall14@gmail.com>
2023-12-06 22:43:35 +0200

    implement Room parameters

--- app/src/main/java/com/example/inventory/data/Item.kt
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -25,7 +25,8 @@ import androidx.room.PrimaryKey
  */
 @Entity(tableName = "items")
 data class Item(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
     val name: String,
     val price: Double,
     val quantity: Int
